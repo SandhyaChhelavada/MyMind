b334525fb206b28228f9b6bc8359905f3e7af668
SandhyaChhelavada <sandhyachhelavada@gmail.com>
2025-06-29 14:11:06 +0530

    Updated App component and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,20 +9,18 @@ import Skills from "./pages/Skills";
 
 const App = () => {
   return (
-    <Router>
-      <div className="d-flex">
-        <Sidebar />
-        <main className="flex-grow-1 ms-5" style={{ paddingLeft: "260px" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/skills" element={<Skills />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="d-flex">
+      <Sidebar />
+      <main className="flex-grow-1 ms-5" style={{ paddingLeft: "260px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { HashRouter } from "react-router-dom"; // ✅ HashRouter used once only
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
